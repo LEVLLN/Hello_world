4bfc467373b320c6b5ccefaee2b1db0580a03c58
Lev <lev.kudryashov1994@gmail.com>
2016-01-08 13:44:21 +0600

    testing

---
+++ app/src/main/java/com/example/user/hello_world/AboutActivity.java
@@ -0,0 +1,35 @@
+package com.example.user.hello_world;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+public class AboutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    public void OneClick(View view) {
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
--- app/src/main/java/com/example/user/hello_world/MainActivity.java
+++ app/src/main/java/com/example/user/hello_world/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.user.hello_world;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
+import android.view.Menu;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -10,39 +12,50 @@ import android.widget.TextView;
 import static com.example.user.hello_world.R.*;
 
 public class MainActivity extends AppCompatActivity {
-private TextView textView;
+    private TextView textView;
     private EditText editText;
     private TextView textView2;
 
-    int i=0;
+    int i = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(layout.activity_main);
-        textView = (TextView)findViewById(id.textView);
-        editText = (EditText)findViewById(id.editText);
-        textView2 = (TextView)findViewById(id.textView2);
+        textView = (TextView) findViewById(id.textView);
+        editText = (EditText) findViewById(id.editText);
+        textView2 = (TextView) findViewById(id.textView2);
 
     }
 
     public void OneClick(View view) {
-         if(editText.getText().length()==0||editText.getText().toString().equals(" ")){
-             textView.setText(string.not_enought_symbol); }
-             else {
-             String strCats = getString(R.string.autohello);
-             String t = "Тапочек";
-             String l = "Лампочка";
+        if (editText.getText().length() == 0 || editText.getText().toString().equals(" ")) {
+            textView.setText(string.not_enought_symbol);
+        } else {
+            String strCats = getString(R.string.autohello);
+            String t = "Тапочек";
+            String l = "Лампочка";
 
-             String finad = String.format(strCats,editText.getText().toString(),t,l);
-             textView.setText(finad);
-                  editText.setText(null);
-             }
+            String finad = String.format(strCats, editText.getText().toString(), t, l);
+            textView.setText(finad);
+            editText.setText(null);
+        }
 
     }
 
     public void CircleButton(View view) {
-        i+=1;
+        i += 1;
         textView2.setHint(String.valueOf(i));
+        Intent intent = new Intent(MainActivity.this, AboutActivity.class);
+        startActivity(intent);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+
 }
