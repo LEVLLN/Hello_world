77567f52bdff2be833cb1f18ba4ff9c121c4e3c8
Lev <lev.kudryashov1994@gmail.com>
2016-01-12 14:47:07 +0600

    12.01.2016

--- app/src/main/java/com/example/user/hello_world/DesignActivity.java
+++ app/src/main/java/com/example/user/hello_world/DesignActivity.java
@@ -4,20 +4,29 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
 
 public class DesignActivity extends AppCompatActivity {
     TextView tv1;
     TextView tv2;
     TextView tv3;
+    TextView tv4;
+    TextView tv5;
+    TextView tv6;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.verstka_layout);
         tv1 = (TextView) findViewById(R.id.contactName);
+        tv5 = (TextView) findViewById(R.id.telephone_name);
+        tv6 = (TextView) findViewById(R.id.telephone_value);
         tv2 = (TextView) findViewById(R.id.personalAccountName);
         tv3 = (TextView) findViewById(R.id.personalAccountId);
+        tv4 = (TextView) findViewById(R.id.like_a_button);
+
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
@@ -33,6 +42,13 @@ public class DesignActivity extends AppCompatActivity {
             finish();
         }
         return true;                            //закрывает данный layout
+    }
+
+    public void SendMoney(View view) {
+        tv1.setText("ЖОПА");
+        tv2.setText("ЖОПА");
+        tv3.setText("ЖОПА");
+
     }
 }
 
